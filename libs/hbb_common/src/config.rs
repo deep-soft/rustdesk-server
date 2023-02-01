8b74e7a4cf82698a040c1cda6c01c4b3787e010d
deep-soft <deep-soft@users.noreply.github.com>
2023-02-01 12:41:18 +0200

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -82,18 +82,18 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &[
-    "rs-ny.rustdesk.com",
-    "rs-sg.rustdesk.com",
-    "rs-cn.rustdesk.com",
+    "rdsk.protoby.ro",
+    "rdsk.protoby.ro",
+    "rdsk.protoby.ro",
 ];
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
-    _ => "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=",
+    _ => "+RZ6lrklELkPx7Wvb9njT1FETBpjiw9Sp4DrsSg3Yz0=",
 };
 
-pub const RENDEZVOUS_PORT: i32 = 21116;
-pub const RELAY_PORT: i32 = 21117;
+pub const RENDEZVOUS_PORT: i32 = 39876;
+pub const RELAY_PORT: i32 = 39877;
 
 macro_rules! serde_field_string {
     ($default_func:ident, $de_func:ident, $default_expr:expr) => {
